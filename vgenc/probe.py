490c583afab87626fb5a5d513e998b7018732763
Vincent Girès <vincent@vincent.bio>
2024-11-20 12:09:34 +0100

    Do not pop window when process is called on windows

--- vgenc/probe.py
+++ vgenc/probe.py
@@ -1,12 +1,21 @@
 from subprocess import run, PIPE
+import subprocess
+import platform
 import json
+
+startupinfo = None
+if platform.system() == 'Windows':
+    # Do not pop window when process is called
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 
 
 def get_movie_size(input_path: str, stream_index: int = 0) -> tuple[int, int]:
     command = [
         'ffprobe', input_path,
         '-show_entries', 'stream=width,height', '-print_format', 'json']
-    output = run(command, check=True, stdout=PIPE).stdout
+    output = run(
+        command, check=True, stdout=PIPE, startupinfo=startupinfo).stdout
     result = json.loads(output.decode())
     size = (
         result['streams'][stream_index]['width'],
@@ -18,7 +27,8 @@ def get_movie_duration(input_path: str, stream_index: int = 0) -> int:
     command = [
         'ffprobe', input_path, '-count_packets',
         '-show_entries', 'stream=nb_read_packets', '-print_format', 'json']
-    output = run(command, check=True, stdout=PIPE).stdout
+    output = run(
+        command, check=True, stdout=PIPE, startupinfo=startupinfo).stdout
     result = json.loads(output.decode())
     value = result['streams'][stream_index]['nb_read_packets']
     return int(value)
@@ -26,14 +36,16 @@ def get_movie_duration(input_path: str, stream_index: int = 0) -> int:
 
 def get_image_size(input_path: str) -> tuple[int, int]:
     command = ['iinfo', input_path]
-    output = run(command, check=True, stdout=PIPE).stdout
+    output = run(
+        command, check=True, stdout=PIPE, startupinfo=startupinfo).stdout
     x, y = output.decode().split(':')[1].split(',')[0].split('x')
     return int(x), int(y)
 
 
 def get_metadata_from_movie(input_path: str) -> dict:
     command = ['ffprobe', input_path, '-show_format', '-print_format', 'json']
-    output = run(command, check=True, stdout=PIPE).stdout
+    output = run(
+        command, check=True, stdout=PIPE, startupinfo=startupinfo).stdout
     result = json.loads(output.decode())
     return result['format']['tags']
 
@@ -56,7 +68,8 @@ def get_metadata_from_image(input_path: str) -> dict:
         return value
 
     command = ['iinfo', '-v', input_path]
-    output = run(command, check=True, stdout=PIPE).stdout
+    output = run(
+        command, check=True, stdout=PIPE, startupinfo=startupinfo).stdout
     return {
         get_key(i): get_value(i)
         for i in output.decode().split('\n') if len(i.split(': ')) > 1}
@@ -64,7 +77,8 @@ def get_metadata_from_image(input_path: str) -> dict:
 
 def get_stream_info(input_path: str) -> dict:
     command = ['ffprobe', input_path, '-show_streams', '-print_format', 'json']
-    output = run(command, check=True, stdout=PIPE).stdout
+    output = run(
+        command, check=True, stdout=PIPE, startupinfo=startupinfo).stdout
     return json.loads(output.decode())
 
 
